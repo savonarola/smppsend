2ff85db06f96fcebc5494b3535f42e0a61647ceb
Ilia Averianov <av@rubybox.dev>
2026-07-09 17:53:35 +0300

    Version bumpup: 0.2.2 -> 0.2.3

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Smppsend.Mixfile do
   def project do
     [
       app: :smppsend,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
